07c00724b628a6c53670abf5febe80ab23d3118f
Victor Martinez <vicmtzdev@gmail.com>
2023-06-16 00:04:31 -0600

    new-design

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { StrangerThingsApp } from './src/strangerthings/strangerthings-app'
 
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 id = "app-title">Hello Vite!</h1>
-    <div class="card">
-     
-    </div>
     
+    <img src="./src/assets/stlogo.png" class="logocustom vanilla"/>
+    <div class="card">
+
+    </div>
+
   </div>
-`
+`;
 
+const element = document.querySelector('.card');
 
+StrangerThingsApp(element);
---
+++ src/strangerthings/strangerthings-app.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const StrangerThingsApp = (element) => {
+
+    element.innerHTML = 'Loading...';
+
+}
